a642d848d8665341aba2d4bddb7dd674fccf20c4
sergiosanchez2 <zergiosanchezlopez@gmail.com>
2022-04-25 13:34:38 +0200

    nuevas imagenes y problema solucionado

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -10,20 +10,22 @@
     <v-row>
       <v-col v-for="proyect in proyects" :key="proyect.name"
       cols="3" class="text-center fade-scroll  d-flex justify-center">
-        <v-card class="mx-auto my-12" width="350" height="450">
-          <!--si ponemos imagenes poner height="450"-->
+        <v-card class="mx-auto my-12" width="350" height="580">
           <template slot="progress">
             <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
           </template>
-          <!-- <v-img height="250" :src="proyect"></v-img> -->
-          <!-- <v-img src="../assets/img/logos/html5.png" height="250"></v-img> -->
-          <v-card-title>{{ proyect.name}}</v-card-title>
-          <v-img v-bind="proyects.img"></v-img>
+          <v-card-title class="d-flex justify-center align-center">
+            <strong class="titulillo display-1">{{ proyect.name}}</strong></v-card-title>
+          <v-img  contain :src="getImg(proyect.img)" :class="proyect.img" class="imagen"></v-img>
+          <div class="d-flex flex-row mb-6">
+            <v-img contain v-for="icon in proyect.tecnologias"
+            :key="icon.tecnologias" :src="getImg(icon)" class="iconitos" ></v-img>
+          </div>
           <v-card-text class="text-justify">
-            <div height="500">{{proyect.descripcion}}</div>
+            <div height="500" class="texto">{{proyect.descripcion}}</div>
           </v-card-text>
           <v-divider class="mx-4"></v-divider>
-          <v-card-actions>
+          <v-card-actions class="d-flex justify-center align-center">
             <v-btn color="primary" large outlined class="btnC">
               Show more
             </v-btn>
@@ -38,43 +40,63 @@ export default {
   name: 'MyProjects',
   data() {
     return {
-      image: {
-        src: 'https://lenguajejs.com/javascript/logo.svg',
-        alt: 'Logo de Javascript',
-      },
       proyects: [
         {
-          img: '../assets/img/capturas/ahorcado.png',
+          img: 'capturas/ahorcadoIcon',
           name: 'Ahorcado',
-          tecnologias: 'css ,javascript y html',
-          descripcion: 'ahorcado que realizado con un array que contiene palabras que tendras que adivinar',
-          link: 'hhh',
+          tecnologias: [
+            'logos/html5',
+            'logos/css3',
+            'logos/javascript',
+          ],
+          descripcion: 'Ahorcado que realizado con un array que contiene palabras que tendras que adivinar.',
+          link: '',
         },
         {
-          img: '',
+          img: 'capturas/trivialIcon',
           name: 'Trivial',
-          tecnologias: 'php csv javascript css y html',
-          descripcion: 'trivial que guarda las preguntas en un csv.100% funcional',
+          tecnologias: [
+            'logos/html5',
+            'logos/css3',
+            'logos/javascript',
+            'logos/php',
+            'logos/csv',
+          ],
+          descripcion: 'Trivial que guarda las preguntas en un csv. 100% funcional.',
           link: '',
         },
         {
-          img: '../assets/img/capturas/calculadora.png',
-          name: 'calculadora',
-          tecnologias: 'javascript css y html ',
-          descripcion: 'calculadora realizada con el pardigma poo. tiene opcion cientifica',
+          img: 'capturas/calculadoraIcon',
+          name: 'Calculadora',
+          tecnologias: [
+            'logos/html5',
+            'logos/css3',
+            'logos/javascript',
+          ],
+          descripcion: 'Calculadora realizada con el paradigma poo. tiene opcion cientifica.',
           link: '',
         },
         {
-          img: '../assets/img/capturas/gestorBD.png',
-          name: 'gestor de alumnos',
-          tecnologias: 'php bootstrap css html y javascript',
-          descripcion: 'gestor de base de datos de una escula de alumnos funcional',
+          img: 'capturas/gesto',
+          name: 'Gestor de alumnos',
+          tecnologias: [
+            'logos/html5',
+            'logos/bootstrap',
+            'logos/javascript',
+            'logos/php',
+            'logos/sql',
+          ],
+          descripcion: 'Gestor de base de datos de una escuela de alumnos funcional.',
           link: '',
         },
       ],
     };
   },
   methods: {
+    getImg(img) {
+      // eslint-disable-next-line
+      return require(`../assets/img/${img}.png`);
+    },
   },
 };
 </script>
