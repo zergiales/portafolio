b7666b9ecde53f88edb6f7b0526f1b0ba65513ea
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2022-09-28 13:17:38 +0200

    menu responsive

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,5 +1,6 @@
 <template id="header">
-  <v-app-bar color="secondaryMedium" light flat class="px-8 hidden-sm-and-down">
+  <v-app>
+    <v-app-bar color="secondaryMedium" light flat class="px-8 hidden-sm-and-down">
         <v-spacer></v-spacer>
         <v-tabs fixed-tabs dark color="primaryLight">
             <v-tab  active-class="active"
@@ -11,10 +12,29 @@
             <v-tab  active-class="active"
             @click="scroll('contact')">Contacto</v-tab>
         </v-tabs>
-  </v-app-bar>
+    </v-app-bar>
+    <Slide class="hidden-md-and-up" width="250">
+        <v-tabs fixed-tabs dark color="primaryLight">
+            <v-tab  active-class="active"
+            @click="scroll('home')">Inicio</v-tab>
+            <v-tab  active-class="active"
+            @click="scroll('proyects')">Proyectos</v-tab>
+            <v-tab  active-class="active"
+            @click="scroll('about')">Sobre mi</v-tab>
+            <v-tab  active-class="active"
+            @click="scroll('contact')">Contacto</v-tab>
+        </v-tabs>
+    </Slide>
+  </v-app>
+
 </template>
 <script>
+import { Slide } from 'vue-burger-menu';
+
 export default {
+  components: {
+    Slide,
+  },
   name: 'MiHeader',
   methods: {
     scroll(section) {
